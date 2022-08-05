a8502714851941aa3179425a11ef665e8088e834
RubenBoard <klsj9810@gmail.com>
2022-08-05 13:43:10 +0900

    day5

--- app.js
+++ app.js
@@ -1,57 +1,7 @@
 const title = document.querySelector("div.hello:first-child h1");
-let count = 0;
 
-console.dir(title);
 function handleTitleClick(){
-    count+=1;
-    if (count%2==0){
-        title.style.color = "blue";
-    } else {
-        title.style.color = "red";
-    };
-}
-
-function handleMouseEnter(){
-    count+=1;
-    if (count%2==0){
-        title.style.color = "blue";
-    } else {
-        title.style.color = "red";
-    };
-
-    title.innerText = "Thank u !"
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is here!"
+    title.classList.toggle("clicked");
 }
 
 title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-// 타이틀을 클릭하는 행위를 리슨한다.
-title.onclick = handleTitleClick;
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-
-window.addEventListener("resize", handleWindowResize);
-
-
-function handleWindowCopy(){
-    alert("copier!")
-}
-window.addEventListener("copy",handleWindowCopy);
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("you are on WIFI!");
-}
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
