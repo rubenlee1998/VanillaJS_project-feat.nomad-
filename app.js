66c1a86776a039d99d5f216613fbb95440c22133
RubenBoard <klsj9810@gmail.com>
2022-08-08 12:53:06 +0900

    delete all

--- app.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    title.classList.toggle("clicked");
-}
-
-title.addEventListener("click", handleTitleClick);
