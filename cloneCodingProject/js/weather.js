4c441765dba5477f1c92b4166fff06d7a1fdba4f
RubenBoard <klsj9810@gmail.com>
2022-08-15 03:21:57 +0900

    Error edit

--- cloneCodingProject/js/weather.js
+++ cloneCodingProject/js/weather.js
@@ -5,9 +5,7 @@ const API_KEY = "46e0d708d67a0689c0b30808f41e8b2c";
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live it", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    console.log(url);
     fetch(url).then(response => response.json())
     .then(data =>{
         const weather = document.querySelector("#weather span:first-child");
